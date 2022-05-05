aa9d708060762703eaace6f969c3ce8cdd106cf4
etorlic <emin.torlic@icloud.com>
2022-05-05 14:09:27 -0600

    Palindrome and mLComment

--- regex_practice.js
+++ regex_practice.js
@@ -8,12 +8,13 @@ const regexes = {
   notThreeEndingInOO: /(()|^([a-zA-Z]+))/,
   divisibleBy32: /^(0+|((0|1)*00000))$/, //FULLY DONE
   sevenThroughThirtyOne: /^([7-9]|[12]\d|3[0-1])$/, //FULLY DONE
-  mLComment: /^([(])\*[^a-zA-z]*\*([)])$/,
+  mLComment: /^([(])\*[^a-zA-Z]*\*([)])$/, //FULLY DONE
   notFileForFirstNoLookAround: /\w?[^(file)|(for)|(first)]/,
   notFileForFirstWithLookAround: /^((?!(^file$|^for$|^first$)).)*$/, //FULLY DONE
   cOctal: /^0[0-7]*$/, //FULLY DONE
   restrictedFloatingPoint: /^-?[0-9]+\.?[0-9]*((E|e)(-|\+)?[0-9]{1,3})?$/, //FULLY DONE
-  palindrome2358: /(^(.).?\2$)|(^(.)(.).\5\4$)|(^(\X{1,2})(.)(.)(.)\10\9\8\7$)/, //ALMOST DONE
+  palindrome2358:
+    /(^(.).?\2$)|(^(.|.+)(.|.+).\5\4$)|(^(.|.+)(.|.+)(.|.+)(.|.+)\10\9\8\7$)/, //FULLY DONE
   noNegativeIntLits: /(?<![-\d])(\d+)/, //FULLY DONE
   repeated: /^([a-z]*)\1$/, //FULLY DONE
 };
